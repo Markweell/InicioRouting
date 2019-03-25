08e60122fe360be6056189c5da8f0d5be096c55f
Marcos Gallardo Pérez <marcos.gallardo@atmira.com>
2019-03-25 08:33:03 +0100

    first-Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './vistas/home/home.component';
+import { ContactoComponent } from './vistas/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/vistas/sobre-nosotros/sobre-nosotros.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SobreNosotrosRoutingModule } from './sobre-nosotros-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SobreNosotrosRoutingModule
+  ]
+})
+export class SobreNosotrosModule { }
